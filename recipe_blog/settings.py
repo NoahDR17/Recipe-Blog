09ca6095e634541d34a5038438c8c3dafe3ec5e2
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2024-10-28 16:45:02 +0000

    switch to postre database, create recipe model

--- recipe_blog/settings.py
+++ recipe_blog/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-if os.path.exists('env.py'):
-    import env 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['8000-noahdr17-recipeblog-0q42eit5qf9.ws-eu116.gitpod.io',
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-noahdr17-recipeblog-0q42eit5qf9.ws-eu116.gitpod.io',
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
     ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
@@ -57,6 +59,7 @@ INSTALLED_APPS = [
 
     # Apps 
     'home',
+    'recipes',
 
     # Other 
     'crispy_forms',
@@ -114,13 +117,16 @@ WSGI_APPLICATION = 'recipe_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#     }
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
---
+++ recipes/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Recipe(models.Model):
+    """
+    The model to create recipes
+    """
+    title = models.CharField(max_length=250, blank=False, null=False,  unique=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_author")
+    description = models.CharField(max_length=500, null=False, blank=False)
+    calories = models.IntegerField()
+    ingredients = models.CharField(max_length=10000, null=False, blank=False)
+    instructions = models.CharField(max_length=10000, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
